e0d6369c9df77350300e105fe3d67834d22f0940
Gaangb <angelob4stos@gmail.com>
2023-10-23 17:37:30 -0300

    refact: melhorando estrutura do welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'HDC Events')
 
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body>
-        <h1>um titulo</h1>
-        <img src="/img/banner.jpg" alt="">
-        @if(10>5)
-            <p>A condicao é atendida</p>
-        @endif
+@section('content')
+<h1>um titulo</h1>
+<img src="/img/banner.jpg" alt="">
+@if(10>5)
+    <p>A condicao é atendida</p>
+@endif
 
-        <p>{{ $nome }}</p>
+<p>{{ $nome }}</p>
 
-        @if($nome == "Pedro")
-            <p>O nome é {{$nome}}</p>
-        @elseif($nome == "Gabriel")
-            <p>O nome é {{$nome}} e ele tem {{$idade}} anos</p>
-        @else
-            <p>O nome não é Pedro</p>
-        @endif
+@if($nome == "Pedro")
+    <p>O nome é {{$nome}}</p>
+@elseif($nome == "Gabriel")
+    <p>O nome é {{$nome}} e ele tem {{$idade}} anos</p>
+@else
+    <p>O nome não é Pedro</p>
+@endif
 
-        @for($i = 0; $i < count($arr); $i++)
-            <p>{{$arr[$i]}}</p>
-        @endfor
+@for($i = 0; $i < count($arr); $i++)
+    <p>{{$arr[$i]}}</p>
+@endfor
 
-        @foreach($nomes as $nome)
-            <p>{{$nome}}</p>
-        @endforeach
-    </body>
-</html>
+@foreach($nomes as $nome)
+    <p>{{$nome}}</p>
+@endforeach
+
+@endsection
