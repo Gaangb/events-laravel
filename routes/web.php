26d399e108aa7e99dea38df31fe7ce6112dd5424
Gaangb <angelob4stos@gmail.com>
2023-10-25 20:24:39 -0300

    feat: criando rota para o event para salvar os dados

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
-
+Route::post('/events', [EventController::class, 'store']);
 Route::get('/contato', [EventController::class, 'contact']);
 
 // Route::get('/produtos', function () {
