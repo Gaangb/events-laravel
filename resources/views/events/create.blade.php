4247f36cee485f92baf3685c7d9cbfb77b38eb13
Gaangb <angelob4stos@gmail.com>
2023-10-25 20:15:40 -0300

    feat: adicionando diretiva para ataques a formulario

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -7,6 +7,7 @@
 <div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Crie o seu evento</h1>
     <form action="/events" method="POST">
+        @csrf
         <div class="form-group">
             <label for="title">Evento:</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Nome do seu evento">
@@ -24,7 +25,7 @@
         </div>
         <div class="form-group">
             <label for="title">Descrição:</label>
-            <textarea name="description" id="description" class="form-control" placeholder="O que Vai acontecer no evento?"></textarea>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
         </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
     </form>
