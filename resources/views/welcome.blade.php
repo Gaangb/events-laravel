2faa7341033276991044fbcaba0d9486a07c8a09
Gaangb <angelob4stos@gmail.com>
2023-10-25 19:29:51 -0300

    ref: alterando pagina inicial para o que será usado no projeto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,28 +3,29 @@
 @section('title', 'HDC Events')
 
 @section('content')
-<h1>um titulo</h1>
-<img src="/img/banner.jpg" alt="">
-@if(10>5)
-    <p>A condicao é atendida</p>
-@endif
 
-<p>{{ $nome }}</p>
-
-@if($nome == "Pedro")
-    <p>O nome é {{$nome}}</p>
-@elseif($nome == "Gabriel")
-    <p>O nome é {{$nome}} e ele tem {{$idade}} anos</p>
-@else
-    <p>O nome não é Pedro</p>
-@endif
-
-@for($i = 0; $i < count($arr); $i++)
-    <p>{{$arr[$i]}}</p>
-@endfor
-
-@foreach($nomes as $nome)
-    <p>{{$nome}}</p>
-@endforeach
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="procurar">
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Proximos eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/event_placeholder.jpg" alt="{{ $event -> title }}">
+                <div class="card-body">
+                    <p class="card-date">10/09/2020</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">X participants</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 
 @endsection
